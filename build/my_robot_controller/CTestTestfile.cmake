856ad7d0f072cf00a3d46d5160cdf200dba10de6
louaymikou <louaymikou17@gmail.com>
2025-12-03 17:28:08 +0100

    first commit

---
+++ build/my_robot_controller/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/wayay/ROS_PROJECT/src/my_robot_controller
+# Build directory: /home/wayay/ROS_PROJECT/build/my_robot_controller
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ install/my_robot_controller/share/my_robot_controller/launch/launch_sim.launch.py
@@ -0,0 +1,59 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+import xacro
+
+def generate_launch_description():
+    pkg_name = 'my_robot_controller'
+
+    # 1. Configurer le robot (URDF)
+    xacro_file = os.path.join(get_package_share_directory(pkg_name), 'description', 'robot.urdf.xacro')
+    robot_description_config = xacro.process_file(xacro_file)
+    robot_description = {'robot_description': robot_description_config.toxml()}
+
+    # 2. Lancer Gazebo (serveur + client)
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+    )
+
+    # 3. Spawner le robot dans Gazebo
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'my_bot'],
+                        output='screen')
+
+    # 4. Robot State Publisher (Publie les transformations)
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[robot_description, {'use_sim_time': True}]
+    )
+
+    # 5. Spawner les contrôleurs (DIFF DRIVE)
+    spawn_diff_drive = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+        output="screen"
+    )
+
+    # 6. Spawner les contrôleurs (JOINT STATE)
+    spawn_joint_broad = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+        output="screen"
+    )
+
+    return LaunchDescription([
+        gazebo,
+        node_robot_state_publisher,
+        spawn_entity,
+        spawn_diff_drive,
+        spawn_joint_broad
+    ])
