0c1ef1d22eec3a75d5c1a525c09707f1de74dad0
Lamiaehadi <lamiaehadi14@gmail.com>
2025-12-20 12:04:40 +0100

    Remove build artifacts from git tracking

--- build/my_robot_controller/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lamiae/ROS2_Project/ROS2_Project/src/my_robot_controller
-# Build directory: /home/lamiae/ROS2_Project/ROS2_Project/build/my_robot_controller
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("my_robot_controller__py")
